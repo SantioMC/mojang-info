d6466ee236536eeebff9717652f8d1e5431ce9cd
Santio <santiothebest123@gmail.com>
2022-05-01 22:56:46 -0400

    Push Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import '../styles/globals.css';
+
+const theme = createTheme({
+	type: 'dark',
+	theme: {
+		colors: {
+			// brand colors
+			background: '#1d1d1d',
+			text: '#ddd',
+			gray700: '#999',
+			selection: '#abe0ff52'
+		}
+	}
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<NextUIProvider theme={theme}>
+			<Component {...pageProps} />
+		</NextUIProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
